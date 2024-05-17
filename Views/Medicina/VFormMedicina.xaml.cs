55ac2750aa93a9acea1241258266fb92fe5e987f
Bryan Vallejo <bryan0289@hotmail.com>
2024-05-16 20:22:03 -0500

    Tests

--- Views/Medicina/VFormMedicina.xaml.cs
+++ Views/Medicina/VFormMedicina.xaml.cs
@@ -6,4 +6,27 @@ public partial class VFormMedicina : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void btnTomarF_Clicked(object sender, EventArgs e)
+    {
+        var foto = await MediaPicker.CapturePhotoAsync();
+
+        if (foto != null)
+        {
+            var memoriaStream = await foto.OpenReadAsync();
+            medicinaF.Source = ImageSource.FromStream(() => memoriaStream);
+        }
+
+    }
+
+    private async void btnSeleccionarF_Clicked(object sender, EventArgs e)
+    {
+        var foto = await MediaPicker.PickPhotoAsync();
+
+        if (foto != null)
+        {
+            var memoriaStream = await foto.OpenReadAsync();
+            medicinaF.Source = ImageSource.FromStream(() => memoriaStream);
+        }
+    }
 }
--- Views/Paciente/VFormPaciente.xaml.cs
+++ Views/Paciente/VFormPaciente.xaml.cs
@@ -7,4 +7,25 @@ public partial class VFormPaciente : ContentPage
 		InitializeComponent();
 	}
 
+    private async void btnTomarF_Clicked(object sender, EventArgs e)
+    {
+        var foto = await MediaPicker.CapturePhotoAsync();
+
+        if (foto != null)
+        {
+            var memoriaStream = await foto.OpenReadAsync();
+            pacienteImg.Source = ImageSource.FromStream(() => memoriaStream);
+        }
+    }
+
+    private async void btnSeleccionarF_Clicked(object sender, EventArgs e)
+    {
+        var foto = await MediaPicker.PickPhotoAsync();
+
+        if (foto != null)
+        {
+            var memoriaStream = await foto.OpenReadAsync();
+            pacienteImg.Source = ImageSource.FromStream(() => memoriaStream);
+        }
+    }
 }
